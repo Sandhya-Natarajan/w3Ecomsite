f0cfdeeb9ac5c7981bffa3b5d6ee184548897974
Sandhya <sandhya.w3dreams@gmail.com>
2025-10-07 11:50:18 +0530

    All the changes done in category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,78 +8,83 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\CategoryCollection;
-
+use Illuminate\Http\Resources\Json\JsonResource;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CategoryController extends Controller
 {
 
-
-    //Display the specified resource.
-    public function category($id)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): JsonResponse
     {
-        $categoryId = $id;
-        $category = Category::find($categoryId);
-        if(!$category) {
-            return response()->json(['message'=>'Category not found'],404);
+        $query = Category::query();
+
+        if (!empty($search = $request->get("search"))) {
+            $query->where("name", "like", "%" . $search . "%")
+                ->orWhere("desc", "like", "%" . $search . "%");
         }
-        return new CategoryResource($category);
+
+        $categories = $query->latest()->paginate($request->get("per_page", 10));
+
+        return response()->json([
+            "data" => CategoryResource::collection($categories),
+            "pagination" => [
+                "current_page" => $categories->currentPage(),
+                "last_page" => $categories->lastPage(),
+                "per_page" => $categories->perPage(),
+                "total" => $categories->total(),
+            ]
+        ], 200);
     }
 
-
-
-    //Display a listing of the resource.
-    public function categories()
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request, Category $category): JsonResponse
     {
-        $categories = Category::all();
-        if($categories->isEmpty()) {
-            return response()->json(['message'=>'Category not found'],404);
-        }
-        return new CategoryCollection($categories);
+        $category = Category::create($request->validated());
+
+        return response()->json([
+            "data" => new CategoryResource($category),
+            "message" => "Category Created Successfully",
+        ], 201);
     }
 
-
-
-    //Store a newly created resource in storage.
-    public function AddCategory(CategoryRequest $request)
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category): JsonResponse
     {
-         $category = Category::create([
-            'name'=> $request['name'],
-            'desc'=> $request['desc']
-        ]);
-
-         return response()->json(['message'=>'Category Created Successfully...']);
-
+        return response()->json([
+            "data" => new CategoryResource($category),
+        ], 200);
     }
 
-
-
-
-    //Update the specified resource in storage.
-    public function UpdateCategory(CategoryRequest $request, $id)
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, Category $category): JsonResponse
     {
-        $category = Category::find($id);
+        $category->update($request->validated());
 
-        $category->update([
-            'name'=> $request['name'],
-            'desc'=> $request['desc']
-        ]);
-
-        return response()->json(['message'=>'Category Updated Successfully...']);
-
-
+        return response()->json([
+            "data" => new CategoryResource($category),
+            "message" => "Category Updated Successfully",
+        ], 201);
     }
 
-
-
-    //Remove the specified resource from storage.
-    public function DeleteCategory($id)
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category): JsonResponse
     {
-        $category = Category::find($id);
         $category->delete();
 
-        return response()->json(['message'=>'Category Deleted Successfully...']);
-
-
+        return response()->json([
+            "message" => "Category Deleted Successfully",
+        ], 200);
     }
-
 }
